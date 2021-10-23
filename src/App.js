938922efddd8a98d7864b26ec0cee826e5af7068
PRASHANT CHOUHAN <pchouhan122@gmail.com>
2021-10-23 11:08:31 +0530

    Create Header file and add style of it.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import Header from './component/Header'
 import Navbar from './component/Navbar'
 
 const App = () => {
   return (
     <>
-     <Navbar/>
+      <Navbar />
+      <Header/>
     </>
   )
 }
